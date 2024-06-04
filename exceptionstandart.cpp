4a325ccdf993ba2d0fe7983e587af29370f672c2
SASCHADDD <saschadanu23@gmail.com>
2024-06-04 18:32:04 +0700

    end

--- exceptionstandart.cpp
+++ exceptionstandart.cpp
@@ -16,6 +16,10 @@ int main(){
     catch (exception& e) {
         //penangkap menggunakan obyek exception
         cout<< e.what() << endl;
-        //akan dieksekusi karna array data hanya
+        //akan dieksekusi karna array data hanya memiliki 3 elemen
     }
+    cout<< "Baris program yang terakhir" << endl;
+    /*penanda 2; bahwa program brjalan tanpa berhenti meskipun
+    terjadi kesalahan*/
+    return 0;
 }
