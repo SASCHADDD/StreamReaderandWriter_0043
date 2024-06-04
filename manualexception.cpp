c354d517b1d59c2c35325ea3d0730ee4ec995e9b
SASCHADDD <saschadanu23@gmail.com>
2024-06-04 18:41:04 +0700

    end

--- manualexception.cpp
+++ manualexception.cpp
@@ -1,9 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    try{
-        cout 
+    try
+    {
+        cout << "Selamat belajar di Prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
+
+    catch (int a)
+    {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+
+    catch (...)
+    {
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
